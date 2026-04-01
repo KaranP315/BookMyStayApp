6cb1b04aa5cfe71bd6f70e6f65f804412d4504a5
KaranP315 <karan2006.p@gmail.com>
2026-04-01 15:05:37 +0530

    uc1

--- App/src/BookMyStayApp.java
+++
@@ -1,5 +0,0 @@
-public class BookMyStayApp {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Book My Stay Application");
-    }
-}
---
+++ App/src/UseCase1.java
@@ -0,0 +1,20 @@
+
+public class UseCase1{
+
+    public static void main(String[] args) {
+
+        System.out.println("------------------------------------------------");
+        System.out.println("Welcome to the Book My Stay App");
+        System.out.println("------------------------------------------------");
+
+        String appName = "Hotel Booking Management System";
+        String version = "v1.0";
+
+        System.out.println("Application Name: " + appName);
+        System.out.println("System Version  : " + version);
+
+        System.out.println("\nStatus: Application initialized and running...");
+        System.out.println("------------------------------------------------");
+
+    }
+}
